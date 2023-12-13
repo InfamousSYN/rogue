ce854286eb1f367dfc8689e2a0ac526f0e2ed4ab
InfamousSYN <ainfamoussyn@gmail.com>
2023-12-13 11:58:47 +0000

    attempting to resolve "No module named 'sphinx_rtd_theme'"

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,10 @@ release = '3.0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
 extensions = [
     "sphinx_rtd_theme",
 ]
-import sphinx_rtd_theme
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
